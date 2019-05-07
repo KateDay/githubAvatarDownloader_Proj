b67c5e39c6448a464f35b250e5e6b4e4389f96e3
Kate <khummle@gmail.com>
2019-05-07 04:02:44 +0000

    printing the Avatar URLs

--- download_avatars.js
+++ download_avatars.js
@@ -2,22 +2,27 @@ var request = require('request');
 var token = require("./secret").GITHUB_TOKEN;
 
 function getRepoContributors(repoOwner, repoName, cb) {
+        
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
-        'User-Agent': 'request'
-        'Authorization': 'token' + token
+        'User-Agent': 'request',
+        'Authorization':'token' + token
       }
     };
   
     request(options, function(err, res, body) {
-      cb(err, body);
+        body = JSON.parse(body);
+        cb(err, body);
     });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+    // console.log("Errors:", err);
+    // console.log("Result:", result);
+    result.forEach(function(repo) {
+        console.log(repo.avatar_url);
+    });
     
 });
 
