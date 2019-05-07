8af1f3b820fd629c7511dac128e4aed60e7e8183
Kate <khummle@gmail.com>
2019-05-07 03:09:31 +0000

    Made the request

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,16 @@
 var request = require('request');
+function getRepoContributors(repoOwner, repoName, cb) {
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+    var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+  }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+    
+  });
+
+  console.log('Welcome to the GitHub Avatar Downloader!');
